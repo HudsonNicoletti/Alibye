7fca2667ef22556b993ca24f9422cb1fa0abb6f9
Hudson Nicoletti <hudnicoletti@icloud.com>
2026-03-24 13:55:54 -0300

    Initial Commit

---
+++ Alibye/AlibyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlibyeApp.swift
+//  Alibye
+//
+//  Created by Hudson Nicoletti on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AlibyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
